ae50dbd488b7931f16490e5ffba132053dcf554f
rohangupta788 <rohangupta788@gmail.com>
2022-02-25 14:03:38 +0530

    First

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles/App.css';
+import { signUpFormValidation } from '../utils/validation';
 
 
 const App = () => {
   
-  return (
+  const intialValues= {name:"",email:"",password:""};
+  const [formValues,setFormValues] = useState(intialValues);
+  const [error,setError] = useState({});
 
+const handleChange =(e)=>{
+const {name,value} = e.target;
+setFormValues({...formValues,[name]:value})
+ }
+
+ const handleSubmit=(e)=>{
+   e.preventDefault();
+  const info = signUpFormValidation(formValues);
+  setError(info);
+ }
+
+  return (
+<form onSubmit={handleSubmit}>
+ <label> Name: </label>
+ <input id="name" name="name" value={formValues.name}
+ onChange={handleChange}
+ />
+ <div>{error.name}</div>
+
+  <label> Email :</label>
+  <input id="email" name="email" value={formValues.email} onChange={handleChange}/>
+  <div>{error.email}</div>
+
+  <label>Password :</label>
+  <input id="password" name="password" value={formValues.password}
+  onChange={handleChange}/>
+  <div>{error.password}</div>
+
+ <label> </label><input id="consent" type="checkbox" />
+ 
+ <button type="submit">Signup</button>
+</form>
   )
 }
 
